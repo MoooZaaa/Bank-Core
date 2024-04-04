79529a242d53b4fce8f70193474734b3befe4024
MoZa <119400846+MoooZaaa@users.noreply.github.com>
2024-04-04 15:14:29 +0200

    Add files via upload

---
+++ TP-DotNet-2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+
+var account = new BankAccount("Constantin", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+// Doc : https://learn.microsoft.com/fr-fr/dotnet/csharp/fundamentals/tutorials/classes#create-your-application
